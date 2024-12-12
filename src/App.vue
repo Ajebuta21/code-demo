35071643a5e08487cc6d38ad9b8c6f9000daaa5f
Elvis <elvisluckyest@gmail.com>
2024-12-12 19:45:28 +0100

    second commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Button } from "@swarmakit/vue";
+</script>
+
+<template>
+  <div>
+    <h1>Code Demo</h1>
+    <Button :label="'Click Me!'" @click="handleClick"></Button>
+  </div>
+</template>
+
+<script lang="ts">
+const handleClick = () => {
+  alert("Button clicked!");
+};
+</script>
